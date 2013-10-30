8199f78451e1f4fbbad45bde014b457c1784db49
Ralph Reid <beresfordjunior@me.com>
2013-10-30 20:51:35 +0000

    Part 3 Functioning-final check

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,4 @@
 module ApplicationHelper
-  def sortable(column, title = nil, sort)
-    title ||= column.titlesize
-    direction =  sort == 'asc' ? 'desc' : 'asc'
-    path = raw ["?", "column=", column, "&sort=", direction].join
-    link_to title, path,  { :id => "#{column.underscore.downcase}_header" }
-  end
+
 
 end
-
-
-  # :id => "#{title}_header"
-
-# id: :title_header
-
-# id: :release_date_header
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,6 +4,13 @@ module MoviesHelper
     count.odd? ?  "odd" :  "even"
   end
 
+  def sortable(column, title = nil, sort)
+    title ||= column.titlesize
+    direction =  sort == 'asc' ? 'desc' : 'asc'
+    path = raw ["?", "column=", column, "&sort=", direction].join
+    link_to title, path,  { :id => "#{column.underscore.downcase}_header" }
+  end
+
   def custom_check_box_tag selected, rating
     ticked = selected.include?(rating)
     check_box_tag "ratings[#{rating}]", 1, ticked
